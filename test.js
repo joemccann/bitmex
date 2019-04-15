ec46c41a7c203fb77dd373e4b2ad9afae70689e3
joemccann <joseph.isaac@gmail.com>
2019-04-15 08:59:55 -0700

    support env vars for api key/secret; fail test for deprecated endpoint

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 const test = require('tape')
 const Bitmex = require('.')
-const bitmex = new Bitmex()
+
+const { env: {
+  API_KEY,
+  API_SECRET
+} } = process
+
+const bitmex = new Bitmex({ API_KEY, API_SECRET })
 
 //
 // Create a mock request and response method
@@ -11,7 +17,13 @@ test('sanity', t => {
   t.end()
 })
 
-const endpoints = ['funding', 'instrument', 'orderBook/L2']
+test('pass - API Key and Secret exists', t => {
+  t.ok(API_KEY)
+  t.ok(API_SECRET)
+  t.end()
+})
+
+const endpoints = ['funding', 'instrument', 'orderBook/L2', 'quote']
 
 test('pass - funding', async t => {
   const endpoint = endpoints[0]
@@ -54,5 +66,19 @@ test('fail - instrument', async t => {
   t.ok(err)
   t.equals(err, `Not Found`)
   t.ok(!data)
+  t.end()
+})
+
+test('fail - quote (is deprecated)', async t => {
+  const endpoint = endpoints[3]
+  const { err, data } = await bitmex.request({
+    body: {
+      symbol: '.BXBT'
+    },
+    endpoint
+  })
+  t.ok(!data)
+  t.ok(err)
+  t.equals(err, 'Forbidden')
   t.end()
 })
